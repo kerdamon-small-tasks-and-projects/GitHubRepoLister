33e88d2b81e6660489c1120f2559abcd6f95e5cd
Konrad Walas <kwalas314@gmail.com>
2021-04-23 22:35:17 +0200

    Change blueprint name and add filtering to returned data

--- controllers.py
+++ controllers.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, request, jsonify
 import requests
 
-rep = Blueprint('repos', __name__, url_prefix='/api/repos')
+rep = Blueprint('api', __name__, url_prefix='/api')
 
-@rep.route('/<username>')
-def hello(username):
-    return jsonify(requests.get(f'https://api.github.com/users/{username}/repos').json())
+@rep.route('/repos/<username>')
+def list_user_repos(username):
+    repos_info = requests.get(f'https://api.github.com/users/{username}/repos').json()
+    repos = {}
+    for repo_info in repos_info:
+        repos[repo_info['name']] = repo_info['stargazers_count']
+
+    return repos
+
