28ebba4cd5f65d8683bb19ddadf1c60246c7814a
Konrad Walas <kwalas314@gmail.com>
2021-04-25 23:38:42 +0200

    Add error handling

--- controllers.py
+++ controllers.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, jsonify
-from services import get_user_repos, get_sum_of_stars_for_user_repos
+from services import get_user_repos, get_sum_of_stars_for_user_repos, UserNotFoundException
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/repos/<username>')
 def list_user_repos(username):
-    return jsonify(get_user_repos(username))
+    try:
+        return jsonify(get_user_repos(username))
+    except UserNotFoundException:
+        return {"message": 'Not Found'}, 404
 
-@api.route('/starSum/<username>')
+@api.route('/stars_sum/<username>')
 def list_sum_of_stars_for_user_repos(username):
-    return {'sum': get_sum_of_stars_for_user_repos(username)}
+    try:
+        return {'sum': get_sum_of_stars_for_user_repos(username)}
+    except UserNotFoundException:
+        return {"message": 'Not Found'}, 404
--- services.py
+++ services.py
@@ -1,7 +1,11 @@
 import requests
 
 def get_user_repos(username):
-    repos_info = requests.get(f'https://api.github.com/users/{username}/repos').json()
+    response = requests.get(f'https://api.github.com/users/{username}/repos')
+    if(response.status_code == 404):
+        raise UserNotFoundException()
+
+    repos_info = response.json()
     repos = []
     for repo_info in repos_info:
         repos.append({'name': repo_info['name'], 'stars': repo_info['stargazers_count']})
@@ -11,3 +15,6 @@ def get_user_repos(username):
 def get_sum_of_stars_for_user_repos(username):
     repos = get_user_repos(username)
     return sum([repo['stars'] for repo in repos])
+
+class UserNotFoundException(Exception):
+    pass
